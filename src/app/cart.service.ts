10bdde0afbe5057b185831ab75a357a1ab9dbdca
newwade <79733618+newwade@users.noreply.github.com>
2023-02-28 13:17:50 +0530

    2 files modified, 2 added

---
+++ src/app/cart.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Product } from './products';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  items: Product[] = [];
+
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getCartItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+}
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { CartService } from '../cart.service';
 import { products, Product } from '../products';
 
 @Component({
@@ -9,7 +10,15 @@ import { products, Product } from '../products';
 })
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Product is added to the cart');
+  }
 
   ngOnInit() {
     const routeParam = this.route.snapshot.paramMap;
