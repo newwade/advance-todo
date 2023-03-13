137ba32d03e7a099abe20e0a98055996454ef3c4
newwade <79733618+newwade@users.noreply.github.com>
2023-03-13 18:43:53 +0530

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -7,5 +8,19 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent {
   items = this.cartService.getCartItems();
-  constructor(private cartService: CartService) {}
+  checkoutform = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.log('Your order has been submitted', this.checkoutform.value);
+    this.checkoutform.reset();
+  }
 }
